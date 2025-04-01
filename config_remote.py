1b09dd417eb9a7386afaa112f62d86bf297f4aa5
Illia Ivashchenko <illia.ivashchenko1@gmail.com>
2025-03-31 22:11:10 -0400

    Adding multiple recipients

--- config_remote.py
+++ config_remote.py
@@ -4,7 +4,8 @@ SMTP_SERVER = os.getenv("SMTP_SERVER")
 SMTP_PORT = int(os.getenv("SMTP_PORT", 587))
 YOUR_EMAIL = os.getenv("YOUR_EMAIL")
 YOUR_APP_PASSWORD = os.getenv("YOUR_APP_PASSWORD")
-RECIPIENT_EMAIL = os.getenv("RECIPIENT_EMAIL")
+MAIN_RECIPIENT = os.getenv("MAIN_RECIPIENT")
+RECIPIENT_EMAILS = [email.strip() for email in os.getenv("RECIPIENT_EMAILS", "").split(",") if email]
 
 STOCKS = ["AAPL", "MSFT", "TSLA", "^GSPC", "^IXIC"]
 
